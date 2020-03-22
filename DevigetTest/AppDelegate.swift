a7056fd7c5b79f31ac10b71ef544119adbf40446
David Diego Gomez <diegodavid@icloud.com>
2020-03-22 20:06:53 -0300

    Initial Commit

---
+++ DevigetTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DevigetTest
+//
+//  Created by David Diego Gomez on 22/03/2020.
+//  Copyright © 2020 David Diego Gomez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DevigetTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DevigetTest
+//
+//  Created by David Diego Gomez on 22/03/2020.
+//  Copyright © 2020 David Diego Gomez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
